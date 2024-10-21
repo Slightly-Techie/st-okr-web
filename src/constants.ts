0145aa94fcad66e24ccdaf0ac29f3cd83f4cc23a
saeedyussif663 <saeedyussif663@gmail.com>
2024-10-21 14:54:44 +0000

    fixed error

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const packages = [
+import { Packages } from "./types";
+
+export const packages: Packages[] = [
   {
     name: "Starter",
     desc: "Get basic tools to create your organization a with up to 10 members",
@@ -17,4 +19,17 @@ export const packages = [
     type: "Premium",
     path: "/brifecase-tick.svg",
   },
+];
+
+export const companyType = [
+  "All",
+  "HealthCare",
+  "Finance",
+  "E-commerce",
+  "Real Estate",
+  "Marketing",
+  "Marketing",
+  "Marketing",
+  "Marketing",
+  "Marketing",
 ];
--- src/pages/JoinCompany.tsx
+++ src/pages/JoinCompany.tsx
@@ -1,13 +1,14 @@
 import Logo from "@/assets/Logo.svg";
+import CompanyTypes from "@/components/companies/companyTypes";
+import Organizations from "@/components/companies/organizations";
+import Packages from "@/components/companies/packages";
 import PageWrapper from "@/components/ui/wrapper";
-import { packages } from "@/constants";
 import { Search } from "lucide-react";
-import { Link } from "react-router-dom";
 
 export default function JoinCompany() {
   return (
     <PageWrapper title="Join Company - OKR">
-      <section className="px-4 md:px-8 flex flex-col mx-auto items-center">
+      <section className="px-4 md:px-8 pb-20  flex flex-col mx-auto items-center">
         <img src={Logo} alt="Logo" className="pt-8 self-start" />
         <section className="mt-7 max-w-screen-lg mx-auto">
           <div>
@@ -23,11 +24,11 @@ export default function JoinCompany() {
             </p>
           </div>
           <Packages />
-          <main className="mt-8 max-w-[800px] border border-black/10 rounded-md">
-            <div>
+          <main className="mt-8 w-screen mr-4 md:max-w-[800px] border border-black/10 rounded-lg">
+            <div className="w-full border-b">
               <div className="w-full relative  border-b border-black/10">
                 <input
-                  className="w-full bg-[#F4F4F4] outline-none py-3 px-11"
+                  className="w-full bg-[#F4F4F4] rounded-t-lg outline-none py-3 px-11"
                   placeholder="Search Organization"
                 />
                 <Search
@@ -35,42 +36,13 @@ export default function JoinCompany() {
                   className="absolute opacity-60 left-4 bottom-[15px]"
                 />
               </div>
-              <div>categories</div>
+              <CompanyTypes />
             </div>
-            {/* search results */}
-            <article></article>
+
+            <Organizations />
           </main>
         </section>
       </section>
     </PageWrapper>
-  );
-}
-
-function Packages() {
-  return (
-    <article className="mt-[30px] max-w-[800px] w-full flex-wrap justify-center md:justify-between flex gap-5">
-      {packages.map((item, index) => (
-        <Link
-          to="/create-company"
-          key={index}
-          className="bg-[#F4F4F4] first:border-[#047358] border border-black/5 py-4 flex-shrink-0 flex-grow-0  pr-2.5 pl-4 w-[245px]  rounded-[16px]"
-        >
-          <div className="flex items-center justify-between">
-            <div className="size-8 border flex items-center justify-center border-black/10 bg-white rounded-md">
-              <img src={item.path} alt={item.name} />
-            </div>
-            <div
-              className={`py-2 ${index === 0 && "text-[#047358]"} px-2.5 text-xs  bg-white border rounded-[4px] border-black/5`}
-            >
-              {item.type}
-            </div>
-          </div>
-          <div className="mt-[11px]">
-            <h5 className="font-bold">{item.name}</h5>
-            <p className="mt-[5px] text-xs text-black/50">{item.desc}</p>
-          </div>
-        </Link>
-      ))}
-    </article>
   );
 }
