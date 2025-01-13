88c3f26563db4726cfb38e547f73487a9f96055b
Numo <numofran6@gmail.com>
2025-01-13 19:25:54 +0000

    Cleanup homepage

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,13 +1,9 @@
 import PageWrapper from "@/components/ui/wrapper";
-// import PerformanceChart from "../charts/PerformanceChart";
-import ProgressChart from "../charts/ProgressChart";
 
 function HomePage() {
   return (
     <PageWrapper title="OKR">
       <div className="bg-white">HomePage</div>
-
-      <ProgressChart />
     </PageWrapper>
   );
 }
