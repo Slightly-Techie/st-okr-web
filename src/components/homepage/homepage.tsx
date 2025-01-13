74a70dcc21651ccb96a7c2e9e6f10fab62cfad5c
Numo <numofran6@gmail.com>
2025-01-13 19:25:06 +0000

    Move Progress Chart into /charts folder

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,9 +1,13 @@
 import PageWrapper from "@/components/ui/wrapper";
+// import PerformanceChart from "../charts/PerformanceChart";
+import ProgressChart from "../charts/ProgressChart";
 
 function HomePage() {
   return (
     <PageWrapper title="OKR">
       <div className="bg-white">HomePage</div>
+
+      <ProgressChart />
     </PageWrapper>
   );
 }
